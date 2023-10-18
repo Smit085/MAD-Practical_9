b03d102ab08e8d56bd05a04bfd9715ca0c00b2e1
Smit <2001smitpatel@gmail.com~>
2023-10-18 12:49:55 +0530

    i

--- app/src/main/java/com/example/mad_app085_p9/MainActivity.kt
+++ app/src/main/java/com/example/mad_app085_p9/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.mad_app085_p9
 
+import android.graphics.drawable.AnimationDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.mad_app085_p9.databinding.ActivityMainBinding
+import com.example.mad_app085_p9.databinding.ActivitySplashBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    lateinit var alarmimages_frameByFrameAnimation : AnimationDrawable
+    lateinit var heart_frameByFrameAnimation : AnimationDrawable
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        binding.imgAlarm.setBackgroundResource(R.drawable.alarm_image_list)
+
+        binding.imgHeart.setBackgroundResource(R.drawable.heart_image_list)
+
+        alarmimages_frameByFrameAnimation = binding.imgAlarm.background as AnimationDrawable
+
+        heart_frameByFrameAnimation = binding.imgHeart.background as AnimationDrawable
+    }
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+
+        if(hasFocus){
+            alarmimages_frameByFrameAnimation.start()
+
+            heart_frameByFrameAnimation.start()
+        }
     }
 }
---
+++ app/src/main/java/com/example/mad_app085_p9/SplashActivity.kt
@@ -0,0 +1,58 @@
+package com.example.mad_app085_p9
+
+import android.content.Intent
+import android.graphics.drawable.AnimationDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import com.example.mad_app085_p9.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity(), Animation.AnimationListener {
+    private lateinit var binding: ActivitySplashBinding
+    private lateinit var guni_framebyframeanimation: AnimationDrawable
+    private lateinit var twinanimation: Animation
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.imgLogo.setBackgroundResource(R.drawable.uvpce_image_list)
+        guni_framebyframeanimation=binding.imgLogo.background as AnimationDrawable
+        twinanimation= AnimationUtils.loadAnimation(this,R.anim.grow)
+        twinanimation.setAnimationListener(this)
+    }
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        if(hasFocus){
+           guni_framebyframeanimation.start()
+            binding.imgLogo.startAnimation(twinanimation)
+        }
+        else
+        {
+            guni_framebyframeanimation.stop()
+        }
+    }
+
+    override fun onAnimationStart(p0: Animation?) {
+
+    }
+
+    override fun onAnimationEnd(p0: Animation?) {
+        Intent(this,MainActivity::class.java).also { startActivity(it)}
+        overridePendingTransition(R.anim.scale_center_out,R.anim.scale_center_in)
+        finish()
+
+    }
+
+    override fun onAnimationRepeat(p0: Animation?) {
+
+    }
+
+}
+
